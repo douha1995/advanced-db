65d7c4c1c26d81f7ebf7b6e6bf9b12fafb284fb5
Abdelrahman Mamdouh <abdodouha1995@gmail.com>
2025-04-20 21:05:58 +0200

    add cursor file

---
+++ sql_cursor.sql
@@ -0,0 +1,199 @@
+------------------- SQL Cursor ------------------------------
+/*
+DECLARE cursor_name CURSOR
+    FOR select_statement;
+
+
+OPEN cursor_name;
+
+FETCH NEXT FROM cursor INTO variable_list;
+
+WHILE @@FETCH_STATUS = 0  
+    BEGIN
+        FETCH NEXT FROM cursor_name;  
+    END;
+
+CLOSE cursor_name;
+
+DEALLOCATE cursor_name;
+*/
+
+
+DECLARE 
+    @product_name VARCHAR(MAX), 
+    @list_price   DECIMAL;
+
+DECLARE cursor_product CURSOR
+FOR SELECT 
+        product_name, 
+        list_price
+    FROM 
+        production.products;
+
+OPEN cursor_product;
+FETCH NEXT FROM cursor_product INTO 
+    @product_name, 
+    @list_price;
+
+WHILE @@FETCH_STATUS = 0
+    BEGIN
+		if @list_price <1000000 
+			begin
+				continue;
+			end
+        PRINT @product_name + CAST(@list_price AS varchar);
+        FETCH NEXT FROM cursor_product INTO 
+            @product_name, 
+            @list_price;
+    END;
+CLOSE cursor_product;
+DEALLOCATE cursor_product;
+
+
+----------------- TRY CATCH ----------------
+/*
+BEGIN TRY  
+   -- statements that may cause exceptions
+END TRY  
+
+BEGIN CATCH  
+   -- statements that handle exception
+END CATCH  
+
+The CATCH block functions
+Inside the CATCH block, you can use the following functions to get the detailed information 
+on the error that occurred:
+
+ERROR_LINE() returns the line number on which the exception occurred.
+ERROR_MESSAGE() returns the complete text of the generated error message.
+ERROR_PROCEDURE() returns the name of the stored procedure or trigger where the error occurred.
+ERROR_NUMBER() returns the number of the error that occurred.
+ERROR_SEVERITY() returns the severity level of the error that occurred.
+ERROR_STATE() returns the state number of the error that occurred.
+Note that you only use these functions in the CATCH block. If you use them outside of 
+the CATCH block, all of these functions will return NULL.
+
+*/
+
+CREATE PROC usp_divide(
+    @a decimal,
+    @b decimal,
+    @c decimal output
+) AS
+BEGIN
+    BEGIN TRY
+        SET @c = @a / @b;
+    END TRY
+    BEGIN CATCH
+        SELECT  
+            ERROR_NUMBER() AS ErrorNumber  
+            ,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS ErrorMessage;  
+    END CATCH
+END;
+GO
+
+DECLARE @r decimal;
+EXEC usp_divide 10, 2, @r output;
+PRINT @r;
+
+
+DECLARE @r2 decimal;
+EXEC usp_divide 10, 0, @r2 output;
+PRINT @r2;
+
+/*
+Inside a CATCH block, you can test the state of transactions by using the XACT_STATE() function.
+
+If the XACT_STATE() function returns -1, it means that an uncommittable transaction is pending,
+you should issue a ROLLBACK TRANSACTION statement.
+In case the XACT_STATE() function returns 1, it means that a committable transaction is pending.
+You can issue a COMMIT TRANSACTION statement in this case.
+If the XACT_STATE() function return 0, it means no transaction is pending, therefore, 
+you don’t need to take any action.
+*/
+
+CREATE TABLE sales.persons
+(
+    person_id  INT
+    PRIMARY KEY IDENTITY, 
+    first_name NVARCHAR(100) NOT NULL, 
+    last_name  NVARCHAR(100) NOT NULL
+);
+
+CREATE TABLE sales.deals
+(
+    deal_id   INT
+    PRIMARY KEY IDENTITY, 
+    person_id INT NOT NULL, 
+    deal_note NVARCHAR(100), 
+    FOREIGN KEY(person_id) REFERENCES sales.persons(
+    person_id)
+);
+
+insert into 
+    sales.persons(first_name, last_name)
+values
+    ('John','Doe'),
+    ('Jane','Doe');
+
+insert into 
+    sales.deals(person_id, deal_note)
+values
+    (1,'Deal for John Doe');
+
+
+CREATE PROC usp_report_error
+AS
+    SELECT   
+        ERROR_NUMBER() AS ErrorNumber  
+        ,ERROR_SEVERITY() AS ErrorSeverity  
+        ,ERROR_STATE() AS ErrorState  
+        ,ERROR_LINE () AS ErrorLine  
+        ,ERROR_PROCEDURE() AS ErrorProcedure  
+        ,ERROR_MESSAGE() AS ErrorMessage;  
+GO
+
+
+CREATE PROC usp_delete_person(
+    @person_id INT
+) AS
+BEGIN
+    BEGIN TRY
+        BEGIN TRANSACTION;
+        -- delete the person
+        DELETE FROM sales.persons 
+        WHERE person_id = @person_id;
+        -- if DELETE succeeds, commit the transaction
+        COMMIT TRANSACTION;  
+    END TRY
+    BEGIN CATCH
+        -- report exception
+        EXEC usp_report_error;
+        
+        -- Test if the transaction is uncommittable.  
+        IF (XACT_STATE()) = -1  
+        BEGIN  
+            PRINT  N'The transaction is in an uncommittable state.' +  
+                    'Rolling back transaction.'  
+            ROLLBACK TRANSACTION;  
+        END;  
+        
+        -- Test if the transaction is committable.  
+        IF (XACT_STATE()) = 1  
+        BEGIN  
+            PRINT N'The transaction is committable.' +  
+                'Committing transaction.'  
+            COMMIT TRANSACTION;     
+        END;  
+    END CATCH
+END;
+GO
+
+
+EXEC usp_delete_person 2;
+
+EXEC usp_delete_person 1;
